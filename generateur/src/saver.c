e230ca71138a239fa494c93dd538d5f73fc91ad2
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2016-05-26 18:04:53 +0200

    ca marche mieux

--- generateur/src/saver.c
+++ generateur/src/saver.c
@@ -5,7 +5,7 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Wed Apr 27 16:58:41 2016 Baptiste veyssiere
-** Last update Sun May 22 18:14:28 2016 Baptiste veyssiere
+** Last update Thu May 26 18:04:19 2016 Baptiste veyssiere
 */
 
 #include <unistd.h>
@@ -21,9 +21,9 @@ int		saver(t_cell ***maze)
     {
       j = 0;
       while (maze[i][++j + 1] != NULL)
-	if (write(0, &(maze[i][j]->wall), 1) == -1)
+	if (write(1, &(maze[i][j]->wall), 1) == -1)
 	  return (-1);
-      if (maze[i + 2] != NULL && write(0, "\n", 1) == -1)
+      if (maze[i + 2] != NULL && write(1, "\n", 1) == -1)
 	return (-1);
     }
   return (0);
